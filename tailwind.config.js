c3e52626354e81916f44eabd37e7d4eda0af2f74
h-bcode <jon.are.br@gmail.com>
2025-01-18 12:13:39 +0100

    added typography and checked with Wave evaluation tool

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,174 +12,84 @@ export default {
       },
       fontSize: {
         // Headings
-        'h1-desktop': [
-          '64px',
-          { lineHeight: '80px', letterSpacing: '-0.5px', fontWeight: '700' },
-        ],
-        'h2-desktop': [
-          '40px',
-          { lineHeight: '48px', letterSpacing: '-0.25px', fontWeight: '700' },
-        ],
-        'h3-desktop': [
-          '24px',
-          { lineHeight: '32px', letterSpacing: '0px', fontWeight: '400' },
-        ],
+        'h1-desktop': ['64px', { lineHeight: '80px', letterSpacing: '-0.03125em' }],
+        'h2-desktop': ['40px', { lineHeight: '48px', letterSpacing: '-0.01563em' }],
+        'h3-desktop': ['24px', { lineHeight: '32px', letterSpacing: '0' }],
 
-        'h1-mobile': [
-          '48px',
-          { lineHeight: '60px', letterSpacing: '-0.5px', fontWeight: '700' },
-        ],
-        'h2-mobile': [
-          '32px',
-          { lineHeight: '40px', letterSpacing: '-0.25px', fontWeight: '700' },
-        ],
-        'h3-mobile': [
-          '20px',
-          { lineHeight: '28px', letterSpacing: '0px', fontWeight: '400' },
-        ],
+        'h1-mobile': ['48px', { lineHeight: '60px', letterSpacing: '-0.03125em' }],
+        'h2-mobile': ['32px', { lineHeight: '40px', letterSpacing: '-0.01563em' }],
+        'h3-mobile': ['20px', { lineHeight: '28px', letterSpacing: '0' }],
 
         // Ingress
-        'ingress-desktop': [
-          '20px',
-          { lineHeight: '28px', letterSpacing: '0px', fontWeight: '400' },
-        ],
-        'ingress-mobile': [
-          '16px',
-          { lineHeight: '24px', letterSpacing: '0px', fontWeight: '400' },
-        ],
+        'ingress-desktop': ['20px', { lineHeight: '28px', letterSpacing: '0' }],
+        'ingress-mobile': ['16px', { lineHeight: '24px', letterSpacing: '0' }],
 
         // Body text
-        'body-large-desktop': [
-          '16px',
-          { lineHeight: '24px', letterSpacing: '0px', fontWeight: '400' },
-        ],
-        'body-medium-desktop': [
-          '14px',
-          { lineHeight: '20px', letterSpacing: '0px', fontWeight: '400' },
-        ],
-        'body-small-desktop': [
-          '12px',
-          { lineHeight: '20px', letterSpacing: '0px', fontWeight: '400' },
-        ],
+        'body-large-desktop': ['16px', { lineHeight: '24px', letterSpacing: '0' }],
+        'body-medium-desktop': ['14px', { lineHeight: '20px', letterSpacing: '0' }],
+        'body-small-desktop': ['12px', { lineHeight: '16px', letterSpacing: '0' }],
 
-        'body-large-mobile': [
-          '14px',
-          { lineHeight: '20px', letterSpacing: '0px', fontWeight: '400' },
-        ],
-        'body-small-mobile': [
-          '12px',
-          { lineHeight: '16px', letterSpacing: '0px', fontWeight: '400' },
-        ],
+        'body-large-mobile': ['14px', { lineHeight: '20px', letterSpacing: '0' }],
+        'body-small-mobile': ['12px', { lineHeight: '16px', letterSpacing: '0' }],
 
         // Buttons
-        'button-primary-desktop': [
-          '14px',
-          { lineHeight: '20px', letterSpacing: '1px', fontWeight: '700' },
-        ],
-        'button-secondary-desktop': [
-          '14px',
-          { lineHeight: '20px', letterSpacing: '1px', fontWeight: '400' },
-        ],
+        'button-primary-desktop': ['14px', { lineHeight: '20px', letterSpacing: '0.0625em' }],
+        'button-secondary-desktop': ['14px', { lineHeight: '20px', letterSpacing: '0.0625em' }],
 
-        'button-primary-mobile': [
-          '12px',
-          { lineHeight: '16px', letterSpacing: '1px', fontWeight: '700' },
-        ],
-        'button-secondary-mobile': [
-          '12px',
-          { lineHeight: '16px', letterSpacing: '1px', fontWeight: '400' },
-        ],
-
-        // Menu
-        'menu-main-desktop': [
-          '14px',
-          { lineHeight: '20px', letterSpacing: '0.5px', fontWeight: '700' },
-        ],
-        'menu-sub-desktop': [
-          '12px',
-          { lineHeight: '16px', letterSpacing: '0.5px', fontWeight: '400' },
-        ],
-
-        'menu-main-mobile': [
-          '14px',
-          { lineHeight: '20px', letterSpacing: '0.5px', fontWeight: '700' },
-        ],
-        'menu-sub-mobile': [
-          '12px',
-          { lineHeight: '16px', letterSpacing: '0.5px', fontWeight: '400' },
-        ],
-
-        // Labels
-        'label-large-desktop': [
-          '12px',
-          { lineHeight: '16px', letterSpacing: '0px', fontWeight: '700' },
-        ],
-        'label-small-mobile': [
-          '10px',
-          { lineHeight: '14px', letterSpacing: '0px', fontWeight: '400' },
-        ],
-
-        // Splash screen
-        'splash-large-desktop': [
-          '48px',
-          { lineHeight: '56px', letterSpacing: '-0.5px', fontWeight: '600' },
-        ],
-        'splash-small-mobile': [
-          '32px',
-          { lineHeight: '40px', letterSpacing: '-0.25px', fontWeight: '600' },
-        ],
+        'button-primary-mobile': ['12px', { lineHeight: '16px', letterSpacing: '0.0625em' }],
+        'button-secondary-mobile': ['12px', { lineHeight: '16px', letterSpacing: '0.0625em' }],
       },
       colors: {
         primary: {
-          DEFAULT: '#49106B', // Primary 01
-          2: '#340C52', // Primary 02
-          3: '#220840', // Primary 03
+          DEFAULT: '#49106B',
+          2: '#340C52',
+          3: '#220840',
         },
         secondary: {
-          DEFAULT: '#7E30E1', // Secondary 01
+          DEFAULT: '#7E30E1',
         },
         neutral: {
-          50: '#FFFFFF', // Pure White
-          100: '#F9FBFE', // Very Light Gray
-          200: '#F4F6FA', // Extra Light Gray
-          300: '#ECEFF4', // Light Gray
-          400: '#F3F8FF', // Neutral 05 (Blueish White)
-          500: '#DCE1E9', // Soft Gray
-          600: '#C8CFD9', // Mid Gray
-          700: '#B3BAC6', // Dark Gray
-          800: '#8A8A8A', // Very Dark Gray
-          900: '#3A3A3A', // Almost Black
+          50: '#FFFFFF',
+          100: '#F9FBFE',
+          200: '#F4F6FA',
+          300: '#ECEFF4',
+          400: '#DCE1E9',
+          500: '#C8CFD9',
+          600: '#B3BAC6',
+          700: '#8A8A8A',
+          800: '#6A6A6A',
+          900: '#3A3A3A',
         },
         accent: {
-          DEFAULT: '#E26EE5', // Accent 01 (Pink)
-          2: '#ECA5E6', // Accent 02 (Lighter Pink)
-          3: '#FFA500', // New Accent (Orange)
-          4: '#FFD700', // New Accent (Gold)
-          discount: '#4CAF50', // Discount (Green)
-          link: '#00BFA6', // Link Color (Turquoise)
+          DEFAULT: '#E26EE5',
+          2: '#ECA5E6',
+          3: '#FFA500',
+          4: '#FFD700',
+          discount: '#4CAF50',
+          link: '#00796B',
         },
         status: {
-          success: '#28A745', // Success Green
-          warning: '#FFC107', // Warning Yellow
-          error: '#DC3545', // Error Red
-          info: '#17A2B8', // Info Blue
+          success: '#28A745',
+          warning: '#FFC107',
+          error: '#DC3545',
+          info: '#17A2B8',
         },
         button: {
-          primary: '#7E30E1', // Button Primary (Purple)
-          secondary: '#E26EE5', // Button Secondary (Pink)
-          hover: '#5D0EAD', // Hover Effect (Darker Purple)
-          hoverSecondary: '#C850C0', // Hover for Secondary (Darker Pink)
-          disabled: '#B3BAC6', // Disabled Button (Gray)
+          primary: '#7E30E1',
+          secondary: '#E26EE5',
+          hover: '#5D0EAD',
+          hoverSecondary: '#C850C0',
+          disabled: '#B3BAC6',
         },
         background: {
-          DEFAULT: '#F9FAFB', // Background color
-          secondary: '#ECEFF4', // Secondary Background
+          DEFAULT: '#F9FAFB',
+          secondary: '#ECEFF4',
         },
         text: {
-          primary: '#1F1F1F', // Primary text color
-          secondary: '#6A6A6A', // Secondary text color
-          muted: '#8A8A8A', // Muted text (for metadata)
-          contrast: '#FFFFFF', // White for dark backgrounds
+          primary: '#1F1F1F',
+          secondary: '#6A6A6A',
+          muted: '#8A8A8A',
+          contrast: '#FFFFFF',
         },
       },
       keyframes: {
