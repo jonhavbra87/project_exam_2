ae7e370d30e4dd4f48aee523e2fdc2fb70c34273
Jon Are <jon.are.br@gmail.com>
2025-02-26 11:52:15 +0100

    added scroll to top on entire site

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import './index.css';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import { StrictMode } from 'react';
+import ScrollToTop from './utilities/ScrollToTop/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+    <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>
